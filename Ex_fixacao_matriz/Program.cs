87762241d66fc3a3e4a2d81b59abef5693b3e332
kalebe-exe <kalebesilvanascimento@gmail.com>
2024-09-02 22:22:20 -0300

    Console.Write atualizado

--- Ex_fixacao_matriz/Program.cs
+++ Ex_fixacao_matriz/Program.cs
@@ -4,7 +4,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Digite a quantidade de linhas e colunas da matriz separados por um espaço: ");
+            Console.Write("Digite a quantidade de linhas e colunas da matriz separados por um espaço: ");
             string[] lxc = Console.ReadLine().Split(' ');
             int linha = int.Parse(lxc[0]);
             int coluna = int.Parse(lxc[1]);
